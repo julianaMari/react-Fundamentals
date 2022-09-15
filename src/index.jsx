78d0b13cad55365f74b536a26267f1d5e29aba05
Juliana Cruz <julianamariacruz.jm@gmail.com>
2022-09-15 00:03:35 -0300

    Corrigindo erro em index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,3 @@
 import React from "react";
-<<<<<<< HEAD
-
-function App() {
-    return <h1>Hello </h1>
-}
-=======
 import { App } from './App'
 
->>>>>>> 8d8159ef0ff0d23ac8e2bbc246fcaa0f64dda781
